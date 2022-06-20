f49a436b30f084137c15b27d7eb1931e8a207976
심하은 <haeun62433@gmail.com>
2022-06-21 01:05:04 +0900

    build error ...

--- App.js
+++ App.js
@@ -1,62 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-//import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {SafeAreaView} from 'react-native';
+import Greeting from './components/Greetings';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
+      <Greeting />
     </SafeAreaView>
   );
 };
---
+++ components/Greetings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Greeting() {
+  return (
+    <View>
+      <Text>안녕하세요 함수 컴포넌트!</Text>
+    </View>
+  );
+}
+export default Greeting;
